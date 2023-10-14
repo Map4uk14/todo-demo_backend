1ad6d46338d41954dc3248f13096560016d7c182
Marks <markshappert@gmail.com>
2023-10-14 12:49:55 +0200

    Initial commit

---
+++ src/main/java/com/example/tododemo/TodoDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.tododemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDemoApplication {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!!!!!");
+
+    SpringApplication.run(TodoDemoApplication.class, args);
+  }
+}
